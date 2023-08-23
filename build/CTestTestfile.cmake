9bf323c36860bf638eda070f21596976fca8f290
TitanHZZ <titanhzz2018@gmail.com>
2023-08-23 15:28:35 +0100

    Removed unused folders.

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/titanhzz/dev/c++/vm
-# Build directory: /home/titanhzz/dev/c++/vm/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
