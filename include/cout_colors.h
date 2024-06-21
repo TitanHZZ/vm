d89bf61031a25b8873f51b7bbc0975951e72e8f2
TitanHZZ <titanhzz2018@gmail.com>
2024-06-21 11:52:20 +0100

    Added support for preprocessor directives and colored printing.

---
+++ include/cout_colors.h
@@ -0,0 +1,35 @@
+#include <iostream>
+
+#ifdef _WIN32
+    #include <windows.h>
+#endif
+
+#ifdef _WIN32
+    #define RED     12
+    #define GREEN   10
+    #define BLUE    9
+    #define YELLOW  14
+#else
+    #define RED     31
+    #define GREEN   32
+    #define BLUE    34
+    #define YELLOW  33
+#endif
+
+void setConsoleColor(int color) {
+    #ifdef _WIN32
+        HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+        SetConsoleTextAttribute(hConsole, color);
+    #else
+        std::cout << "\033[" << color << "m";
+    #endif
+}
+
+void resetConsoleColor() {
+    #ifdef _WIN32
+        HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+        SetConsoleTextAttribute(hConsole, 15); // Reset to default color
+    #else
+        std::cout << "\033[0m";
+    #endif
+}
--- src/Lexer.cpp
+++ src/Lexer.cpp
@@ -3,36 +3,44 @@
 #include <fstream>
 #include <functional>
 
-enum Token_Type {
-    KEYWORD, NUMBER, LABEL, UNKNOWN
-};
+#include "cout_colors.h"
 
-const char *type_as_cstr(Token_Type type) {
-    switch (type) {
-    case Token_Type::KEYWORD:
-        return "KEYWORD";
-    
-    case Token_Type::NUMBER:
-        return "NUMBER";
-    
-    case Token_Type::LABEL:
-        return "LABEL";
-    
-    case Token_Type::UNKNOWN:
-    default:
-        std::cerr << "ERROR: unknown token type" << std::endl;
-        exit(1);
-    }
-}
+enum Token_Type {
+    KEYWORD, NUMBER, LABEL, DIRECTIVE, UNKNOWN
+};
 
 struct Token {
     Token_Type type;
     std::string value;
+
+    size_t line_number;
+    size_t line_offset;
 };
 
 class Lexer {
 public:
     Lexer(const char *path) : path(path) {}
+
+    int print_errors() {
+        int e = 0;
+
+        for (Token &token: tokens) {
+            if (token.type == Token_Type::UNKNOWN) {
+                setConsoleColor(RED); std::cerr << "ERROR: "; resetConsoleColor();
+                setConsoleColor(YELLOW); std::cerr << path << ":" << token.line_number << ":" << token.line_offset; resetConsoleColor();
+                std::cerr << ": Unknown token \"" << token.value << "\"." << std::endl;
+                e++;
+            }
+        }
+
+        return e;
+    }
+
+    void print_tokens() {
+        for (Token &token: tokens) {
+            std::cout << type_as_cstr(token.type) << "\t" << token.value << std::endl;
+        }
+    }
 
     std::vector<Token> tokenize () {
         // open the file
@@ -42,34 +50,49 @@ public:
             exit(1);
         }
 
-        std::vector<Token> tokens;
+        size_t pos_start = pos;
+        size_t line_number = 1;
         while (std::getline(file, line)) {
             pos = 0;
             while (pos < line.size()) {
                 if (std::isspace(line[pos])) {
                     // handle spaces
                     pos++;
+
                 } else if (line[pos] == '#') {
                     // handle comments
                     pos = line.size();
-                } else if (std::isdigit(line[pos])) {
+
+                } else if (line[pos] == '%') {
+                    // handle preprocessor directives
+                    pos++;
+                    const std::string directive = read_while([](char c) { return !std::isspace(c); });
+                    tokens.push_back({Token_Type::DIRECTIVE, std::move(directive), line_number, pos_start});
+
+                } else if (std::isdigit(line[pos]) || line[pos] == '-') {
                     // handle numbers
                     const std::string number = read_while([](char c) { return (c >= 48 && c <= 57) || c == '.' || c == ',' || c == '-' || c == 'e'; });
-                    tokens.push_back({Token_Type::NUMBER, std::move(number)});
+                    tokens.push_back({Token_Type::NUMBER, std::move(number), line_number, pos_start});
+
                 } else if (std::isalpha(line[pos])) {
                     // handle instructions and labels
                     const std::string word = read_while([](char c) { return std::isalnum(c) || c == ':'; });
                     if (word.back() == ':') {
-                        tokens.push_back({LABEL, std::move(word)});
+                        tokens.push_back({LABEL, std::move(word), line_number, pos_start});
                     } else {
-                        tokens.push_back({KEYWORD, std::move(word)});
+                        tokens.push_back({KEYWORD, std::move(word), line_number, pos_start});
                     }
+
                 } else {
                     // handle unknown characters
-                    tokens.push_back({UNKNOWN, std::string(1, line[pos])});
+                    const std::string val = read_while([](char c) { return !std::isspace(c); });
+                    tokens.push_back({UNKNOWN, std::move(val), line_number, pos_start});
                     pos++;
                 }
             }
+
+            line_number++;
+            pos_start = pos;
         }
 
         return tokens;
@@ -84,19 +107,39 @@ private:
         return line.substr(start, pos - start);
     }
 
+    const char *type_as_cstr(Token_Type type) {
+        switch (type) {
+        case Token_Type::KEYWORD:
+            return "KEYWORD  ";
+
+        case Token_Type::NUMBER:
+            return "NUMBER   ";
+
+        case Token_Type::LABEL:
+            return "LABEL    ";
+
+        case Token_Type::DIRECTIVE:
+            return "DIRECTIVE";
+
+        case Token_Type::UNKNOWN:
+        default:
+            std::cerr << "ERROR: Unknown token type." << std::endl;
+            exit(1);
+        }
+    }
+
     const char *path;
     std::string line;
     size_t pos;
+
+    std::vector<Token> tokens;
 };
 
 int main() {
-
-    Lexer lexer("./examples/123i.vasm");
-    std::vector<Token> tokens = lexer.tokenize();
-
-    for (Token &t: tokens) {
-        std::cout << type_as_cstr(t.type) << "\t\t" << t.value << std::endl;
-    }
+    Lexer lexer("./examples/e.vasm");
+    lexer.tokenize();
+    lexer.print_errors();
+    lexer.print_tokens();
 
     return 0;
 }
